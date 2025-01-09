90b446c08225c00b32edbbb57c1ee635405770d9
nprNikita <120847894+nprNikita@users.noreply.github.com>
2025-01-09 10:56:32 +0300

    update main.js

---
+++ main.js
@@ -0,0 +1,6 @@
+async function getFile() {
+  // Open file picker and destructure the result the first handle
+  const [fileHandle] = await window.showOpenFilePicker();
+  const file = await fileHandle.getFile();
+  return file;
+}
